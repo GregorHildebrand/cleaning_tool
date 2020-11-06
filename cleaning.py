1946a18762460f92d08dbcc58901536e53ebe9ae
Gregor Hildebrand <44243728+GregorHildebrand@users.noreply.github.com>
2020-11-06 14:19:02 +0100

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -1,40 +1,20 @@
+#!/usr/bin/env python
+
 # Copyright (c) 2020 Gregor Hildebrand
 
 import os
 
-workdir = os.path.expanduser("~/AppData/Local/Temp/")
-workdir2 = os.path.expanduser("~/AppData/Local/Microsoft/Windows/Explorer/")
-workdir3 = os.path.expanduser("~/AppData/Local/Microsoft/INetCache/IE/")
-workdir4 = os.path.expanduser("~/Windows/Temp/")
-workdir5 = os.path.expanduser("~/AppData/Local/CrashDumps/")
-workdir6 = os.path.expanduser("~/Windows/Logs/")
+workdir0 = os.path.expanduser("~/AppData/Local/Temp/")
+workdir1 = os.path.expanduser("~/AppData/Local/Microsoft/Windows/Explorer/")
+workdir2 = os.path.expanduser("~/AppData/Local/Microsoft/INetCache/IE/")
+workdir3 = os.path.expanduser("~/Windows/TEMP/")
+workdir4 = os.path.expanduser("~/AppData/Local/CrashDumps/")
+workdir5 = os.path.expanduser("~/Windows/Logs/")
 
-if os.path.exists(workdir):
-	for root,dirs,files in os.walk(workdir):
-		for file in files:
-			os.remove(os.path.join(root,file)) # join-method concatenates paths intelligently
+working_array = [workdir0,workdir1,workdir2,workdir3,workdir4,workdir5]
 
-if os.path.exists(workdir2):
-	for root,dirs,files in os.walk(workdir2):
-		for file in files:
-			os.remove(os.path.join(root,file))
-
-if os.path.exists(workdir3):
-	for root,dirs,files in os.walk(workdir3):
-		for file in files:
-			os.remove(os.path.join(root,file))
-
-if os.path.exists(workdir4):
-	for root,dirs,files in os.walk(workdir4):
-		for file in files:
-			os.remove(os.path.join(root,file))
-
-if os.path.exists(workdir5):
-	for root,dirs,files in os.walk(workdir5):
-		for file in files:
-			os.remove(os.path.join(root,file))
-
-if os.path.exists(workdir6):
-	for root,dirs,files in os.walk(workdir6):
-		for file in files:
-			os.remove(os.path.join(root,file))
+for i in range(len(working_array)):
+	if os.path.exists(working_array[i]):
+		for root,dirs,files in os.walk(working_array[i]):
+			for file in files:
+				os.remove(os.path.join(root,file))
