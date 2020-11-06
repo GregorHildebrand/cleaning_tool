55d449aa3dcc6cc1e6b6a38f73cf000d0d3c8883
Gregor Hildebrand <44243728+GregorHildebrand@users.noreply.github.com>
2020-11-06 17:42:39 +0100

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -2,17 +2,21 @@
 
 import os
 
-workdir0 = os.path.expanduser("~/AppData/Local/Temp/")
-workdir1 = os.path.expanduser("~/AppData/Local/Microsoft/Windows/Explorer/")
-workdir2 = os.path.expanduser("~/AppData/Local/Microsoft/INetCache/IE/")
-workdir3 = os.path.expanduser("~/Windows/TEMP/")
-workdir4 = os.path.expanduser("~/AppData/Local/CrashDumps/")
-workdir5 = os.path.expanduser("~/Windows/Logs/")
+def main():
+	workdir0 = os.path.expanduser("~/AppData/Local/Temp/")
+	workdir1 = os.path.expanduser("~/AppData/Local/Microsoft/Windows/Explorer/")
+	workdir2 = os.path.expanduser("~/AppData/Local/Microsoft/INetCache/IE/")
+	workdir3 = os.path.expanduser("~/Windows/TEMP/")
+	workdir4 = os.path.expanduser("~/AppData/Local/CrashDumps/")
+	workdir5 = os.path.expanduser("~/Windows/Logs/")
 
-working_array = [workdir0, workdir1, workdir2, workdir3, workdir4, workdir5]
+	working_array = [workdir0, workdir1, workdir2, workdir3, workdir4, workdir5]
 
-for i in range(len(working_array)):
-	if os.path.exists(working_array[i]):
-		for root, dirs, files in os.walk(working_array[i]):
-			for file in files:
-				os.remove(os.path.join(root, file))
+	for i in range(len(working_array)):
+		if os.path.exists(working_array[i]):
+			for root, dirs, files in os.walk(working_array[i]):
+				for file in files:
+					os.remove(os.path.join(root, file))
+
+	if __name__ == "__main__":
+		main()
