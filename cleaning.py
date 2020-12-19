82f7684690813ee9285e8b9d326b2b91b5df5a34
Gregor Hildebrand <44243728+GregorHildebrand@users.noreply.github.com>
2020-12-19 15:22:04 +0100

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -1,22 +1,14 @@
-# developed in 2020 by: Gregor Hildebrand
+# developed in November 2020 Gregor Hildebrand
 
 import os
 
 def main():
-	workdir0 = os.path.expanduser("~/AppData/Local/Temp/")
-	workdir1 = os.path.expanduser("~/AppData/Local/Microsoft/Windows/Explorer/")
-	workdir2 = os.path.expanduser("~/AppData/Local/Microsoft/INetCache/IE/")
-	workdir3 = os.path.expanduser("~/Windows/TEMP/")
-	workdir4 = os.path.expanduser("~/AppData/Local/CrashDumps/")
-	workdir5 = os.path.expanduser("~/Windows/Logs/")
+	local_app_data = os.path.join(os.environ["LOCALAPPDATA"])
+	local_app_data_tmp = local_app_data + os.sep + "Temp" + os.sep
 
-	working_array = [workdir0, workdir1, workdir2, workdir3, workdir4, workdir5]
-
-	for i in range(len(working_array)):
-		if os.path.exists(working_array[i]):
-			for root, dirs, files in os.walk(working_array[i]):
-				for file in files:
-					os.remove(os.path.join(root, file))
+	for root,dirs,files in os.walk(local_app_data_tmp):
+		for file in files:
+			os.remove(os.path.join(root, file))
 
 if __name__ == "__main__":
 	main()
