34eb45e0a9a61abcd77a8984b525030792ed5c9d
Gregor Hildebrand <44243728+GregorHildebrand@users.noreply.github.com>
2020-12-19 16:21:54 +0100

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -3,12 +3,23 @@
 import os
 
 def main():
-	local_app_data = os.path.join(os.environ["LOCALAPPDATA"])
-	local_app_data_tmp = local_app_data + os.sep + "Temp" + os.sep
+	local_app_data_tmp = os.environ["TEMP"]
+	win_dir = os.environ["Windir"]
+	win_tmp = os.path.join(win_dir, "TEMP")
+	local_app_data = os.environ["LOCALAPPDATA"]
+	local_explorer = os.path.join(local_app_data, "Microsoft","Windows","Explorer")
 
-	for root,dirs,files in os.walk(local_app_data_tmp):
+	for root,dirs,files in os.walk(local_explorer):
 		for file in files:
-			os.remove(os.path.join(root, file))
+			if file.startswith("thumbcache"):
+				os.remove(os.path.join(root, file))
+
+	work_dir = [local_app_data_tmp, win_tmp]
+
+	for i in range(len(work_dir)):
+		for root,dirs,files in os.walk(work_dir[i]):
+			for file in files:
+				os.remove(os.path.join(root, file))
 
 if __name__ == "__main__":
 	main()
